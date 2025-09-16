1262a817f40ed43fa058cf1c0823d64724c94439
vonbergenthedev <vonbergenthedev@gmail.com>
2025-09-16 14:26:17 -0600

    Parsed response into Job Title and URL

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import os
 import requests
-
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 
 load_dotenv()
 
-BUILT_IN_REMOTE_DEV_JAVASCRIPT_ENDPOINT = os.environ.get('BUILT_IN_REMOTE_DEV_JAVASCRIPT')
+response = requests.get(os.environ.get('BUILT_IN_JOBSITE'))
+html_text = response.text
+built_in_jobsite_soup = BeautifulSoup(html_text, "html.parser")
 
-response = requests.get(f'{BUILT_IN_REMOTE_DEV_JAVASCRIPT_ENDPOINT}')
-job_data_text = response.text
-
-
-print(job_data_text)
-
-
-# class BuiltInSiteJobSearch:
-#
-#     def __init__(self):
-#         response = requests.get('BUILT_IN_REMOTE_DEV_JAVASCRIPT_ENDPOINT')
-#         self.job_data = response.json()
-
+for job_card in built_in_jobsite_soup.find_all(name='a', class_="card-alias-after-overlay"):
+    #Only parse if the returned value is headed with /job/ as others are returned in card such as /company/
+    if '/job/' in job_card.get('href'):
+        #Job Title
+        print(f'\n'+job_card.text)
+        #Job URL
+        print(f'https://builtin.com'+job_card.get('href'))
