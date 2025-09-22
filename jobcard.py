0b982e4aed46dac229385514a1c2141117862159
vonbergenthedev <vonbergenthedev@gmail.com>
2025-09-22 09:04:32 -0600

    Add job_id and get method per database discussion.

--- jobcard.py
+++ jobcard.py
@@ -3,9 +3,13 @@ class JobCard:
     def __init__(self, job_title, url):
         self.job_title = job_title
         self.url = url
+        self.job_id = None
 
     def get_job_title(self):
         return self.job_title
 
     def get_url(self):
         return self.url
+
+    def get_job_id(self):
+        return self.job_id
