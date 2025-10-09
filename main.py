e81004928e5fb39a3a8e333d8b2b0bbdb168bfac
vonbergenthedev <vonbergenthedev@gmail.com>
2025-10-09 09:34:23 -0600

    Update main to include initial JSON export. Temporary use of Flask to show JSON response. Update, cleanup, and comment where needed.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import json
+from flask import Flask, Response
 from dotenv import load_dotenv
 from jobsite import Jobsite
 from jobsite_searchterms import search_dict
@@ -8,7 +10,12 @@ from climatebase_jobsite import CBJ
 from workingnomads_jobsite import WNJ
 from techjobsforgood_jobsite import TJFG
 
-## TODO Create Dictionary/JSON export
+site_export = {
+    'builtin': {},  # builtin_jobsite_listings,
+    'climatebase': {},  # climatebase_jobsite_listings,
+    'workingnomads': {},  # workingnomads_jobsite_listings,
+    'techjobsforgood': {},  # techjobsforgood_jobsite_listings,
+}
 
 load_dotenv()
 
@@ -17,24 +24,24 @@ builtin_jobsite = Jobsite(os.environ.get('BUILT_IN_JOBSITE'), exclusions_list=ex
                           search_params=search_dict['builtin']['params'], site_type='static')
 builtin_jobsite_listings = BIJ(builtin_jobsite)
 
-print(f'\n!!**Builtin Job Listings**!!')
-builtin_jobsite_listings.print_jobcard_listings()
+# print(f'\n!!**Builtin Job Listings**!!')
+# builtin_jobsite_listings.print_jobcard_listings()
 
 ## CLIMATE BASE JOBSITE - Dynamic
 climatebase_jobsite = Jobsite(os.environ.get('CLIMATE_BASE_JOBSITE'), exclusions_list=exclusions_jobtitle_list,
                               search_params=search_dict['climatebase']['params'], site_type='dynamic')
 climatebase_jobsite_listings = CBJ(climatebase_jobsite)
 
-print(f'\n!!**Climate Base Job Listings**!!')
-climatebase_jobsite_listings.print_jobcard_listings()
+# print(f'\n!!**Climate Base Job Listings**!!')
+# climatebase_jobsite_listings.print_jobcard_listings()
 
 ## WORKING NOMADS JOBSITE - Dynamic
 workingnomads_jobsite = Jobsite(os.environ.get('WORKING_NOMADS_JOBSITE'), exclusions_list=exclusions_jobtitle_list,
                                 search_params=search_dict['workingnomads']['params'], site_type='dynamic')
 workingnomads_jobsite_listings = WNJ(workingnomads_jobsite)
 
-print(f'\n!!**Working Nomads Job Listings**!!')
-workingnomads_jobsite_listings.print_jobcard_listings()
+# print(f'\n!!**Working Nomads Job Listings**!!')
+# workingnomads_jobsite_listings.print_jobcard_listings()
 
 ## TECH JOBS FOR GOOD JOBSITE - Static
 techjobsforgood_jobsite = Jobsite(os.environ.get('TECH_JOBS_FOR_GOOD_JOBSITE'),
@@ -42,5 +49,52 @@ techjobsforgood_jobsite = Jobsite(os.environ.get('TECH_JOBS_FOR_GOOD_JOBSITE'),
                                   search_params=search_dict['techjobsforgood']['params'], site_type='static')
 techjobsforgood_jobsite_listings = TJFG(techjobsforgood_jobsite)
 
-print(f'\n!!**Tech Jobs for Good Job Listings**!!')
-techjobsforgood_jobsite_listings.print_jobcard_listings()
+# print(f'\n!!**Tech Jobs for Good Job Listings**!!')
+# techjobsforgood_jobsite_listings.print_jobcard_listings()
+
+
+## TODO Create function to compress this down, pass in list of listings instead and iterate over.
+temp_list = []
+counter = 0
+for jobcard in builtin_jobsite_listings.get_listings():
+    site_export['builtin'][counter] = {'job_title': jobcard.get_job_title(), 'job_url': jobcard.get_url(),
+                                       'job_id': jobcard.get_job_id()}
+    counter += 1
+
+counter = 0
+for jobcard in climatebase_jobsite_listings.get_listings():
+    site_export['climatebase'][counter] = {'job_title': jobcard.get_job_title(), 'job_url': jobcard.get_url(),
+                                           'job_id': jobcard.get_job_id()}
+    counter += 1
+
+counter = 0
+for jobcard in workingnomads_jobsite_listings.get_listings():
+    site_export['workingnomads'][counter] = {'job_title': jobcard.get_job_title(), 'job_url': jobcard.get_url(),
+                                             'job_id': jobcard.get_job_id()}
+    counter += 1
+
+counter = 0
+for jobcard in techjobsforgood_jobsite_listings.get_listings():
+    site_export['techjobsforgood'][counter] = {'job_title': jobcard.get_job_title(), 'job_url': jobcard.get_url(),
+                                               'job_id': jobcard.get_job_id()}
+    counter += 1
+
+##
+# TESTING JSON Response
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    payload = site_export
+    response = Response(json.dumps(payload), status=200, mimetype='application/json')
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run()
+##
+
+# print("Initial Export")
+# print(site_export)
